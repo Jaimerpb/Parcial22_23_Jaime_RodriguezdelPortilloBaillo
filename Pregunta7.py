180654b71d74d413fed7923d704f55a4b67fa6d4
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2023-03-30 07:53:46 +0000

    Nodo s

--- Pregunta7.py
+++ Pregunta7.py
@@ -1,2 +1,10 @@
 #Implementar sobre el TDA polinomio desarrollado previamente las siguientes actividades:
 #  restar;  dividir;  eliminar un término;  determinar si un término existe en un polinomio.
+class Node(object):
+    info, sig = None, None
+
+class datopolinomio(object):
+    def __init__(self,valor,termino):
+        self.valor=valor
+        self.termino=termino 
+        
